b1015ac600022d101d9f6fb170375bb48d54194b
Orango Mango <orapama06@gmail.com>
2023-06-11 18:02:14 +0000

    Added link in the editor screen

--- webfx-demo-fooddice-application/src/main/java/com/orangomango/food/ui/NoEditor.java
+++ webfx-demo-fooddice-application/src/main/java/com/orangomango/food/ui/NoEditor.java
@@ -2,12 +2,16 @@ package com.orangomango.food.ui;
 
 import com.orangomango.food.MainApplication;
 import dev.webfx.platform.resource.Resource;
+import dev.webfx.platform.windowlocation.WindowLocation;
+
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
+import javafx.scene.Cursor;
+import javafx.geometry.Rectangle2D;
 
 public class NoEditor{
 	private Image background = MainApplication.loadImage("background_home.jpg");
@@ -16,13 +20,29 @@ public class NoEditor{
 		Canvas canvas = new Canvas(MainApplication.WIDTH, MainApplication.HEIGHT);
 
 		GraphicsContext gc = canvas.getGraphicsContext2D();
+		Rectangle2D bt = new Rectangle2D(50, 300, 75, 75);
 
 		Image homeImage = MainApplication.loadImage("button_home.png");
 		MenuButton home = new MenuButton(() -> {
 			HomeScreen hs = new HomeScreen();
 			MainApplication.setScreen(hs.getLayout());
-		}, 50, 300, 75, 75, homeImage);
-		canvas.setOnMousePressed(e -> home.click(e.getX()/MainApplication.SCALE, e.getY()/MainApplication.SCALE));
+		}, bt.getMinX(), bt.getMinY(), bt.getWidth(), bt.getHeight(), homeImage);
+
+		canvas.setOnMousePressed(e -> {
+			if (bt.contains(e.getX()/MainApplication.SCALE, e.getY()/MainApplication.SCALE)){
+				home.click(e.getX()/MainApplication.SCALE, e.getY()/MainApplication.SCALE);
+			} else {
+				WindowLocation.assignHref("https://orangomango.itch.io/food-dice");
+			}
+		});
+
+		canvas.setOnMouseMoved(e -> {
+			if (bt.contains(e.getX()/MainApplication.SCALE, e.getY()/MainApplication.SCALE)){
+				canvas.setCursor(Cursor.DEFAULT);
+			} else {
+				canvas.setCursor(Cursor.HAND);
+			}
+		});
 
 		MainApplication.onImagesLoaded(() -> {
 			gc.drawImage(this.background, 0, 0, MainApplication.WIDTH, MainApplication.HEIGHT);
